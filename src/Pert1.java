fd0f501abadf84bf9e277d162d2c044300f5db59
XwcdX <darkpuppet90@gmail.com>
2023-08-09 14:25:45 +0700

    init

---
+++ src/Pert1.java
@@ -0,0 +1,12 @@
+public class Pert1{
+    public static void main(String[] args) {
+        System.out.println("---Data Mahasiswa---");
+        System.out.println("Nama : Terry Clement");
+        System.out.println("NRP : c14230074");
+        System.out.println("Mata Kuliah : Algoritma dan Pemrograman");
+        System.out.println("Kelas : B");
+        System.out.println("Tahun : 2023/2024");
+        System.out.println("Semester : Gasal");
+        System.out.println("--------------------");
+    }
+}
